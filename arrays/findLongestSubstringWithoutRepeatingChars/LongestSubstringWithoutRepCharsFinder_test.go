90b6fa6727a2d16c75d815288f35c933822cc382
1mcleancoder <1mcleancoder@gmail.com>
2023-01-12 00:25:13 -0500

    defined the problem, interface and test cases

---
+++ arrays/findLongestSubstringWithoutRepeatingChars/LongestSubstringWithoutRepCharsFinder_test.go
@@ -0,0 +1,51 @@
+package findLongestSubstringWithoutRepeatingChars
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	impls = make([]FindLongestSubstringWithoutRepCharsFinder, 0)
+)
+
+func TestFindLongestSubstringWithoutRepChars(t *testing.T) {
+	impls = append(impls)
+
+	for _, impl := range impls {
+		t.Logf("Using: %T", impl)
+
+		inputs := []struct {
+			info        string
+			input       string
+			expectedOpt int
+		}{
+			{
+				"A positive test case with greatest value is among the area walls",
+				"abccabb",
+				3,
+			},
+			{
+				"A positive test case with greatest value is among the area walls",
+				"aaaaaa",
+				1,
+			},
+			{
+				"Another positive test case with middle value greatest but it is not amongst the wall",
+				"",
+				0,
+			},
+			{
+				"Another positive test case when diff is only one",
+				"abcbda",
+				4,
+			},
+		}
+
+		for i, tt := range inputs {
+			t.Logf("test # %d, input=%v and expected output=%v, %v", i, tt.input, tt.expectedOpt, tt.info)
+			actualOpt := impl.findLongestSubstringWithoutRepeatingChars(tt.input)
+			assert.Equal(t, tt.expectedOpt, actualOpt)
+		}
+	}
+}
---
+++ arrays/findLongestSubstringWithoutRepeatingChars/problem.md
@@ -0,0 +1,11 @@
+Given a string, find the length of longest sub-string without repeating characters.
+
+"abcabb"
+
+Here abc and cab are two strings of same length so longest substring is 3
+
+Constraints:
+- when two strings have same length the return any
+- null or blank string length is zero
+- is the string contiguous? Yes (substring)
+- Does case sensitivity matter? No (all chars are lowercase)

532a6e3f7c37049f97720250e0cd4c42b42daef6
1mcleancoder <1mcleancoder@gmail.com>
2023-01-12 22:59:39 -0500

    small refactoring

--- arrays/findLongestSubstrWithoutRepeatingChars/LongestSubstrWithoutRepCharsFinderS1BF.go
+++ arrays/findLongestSubstrWithoutRepeatingChars/LongestSubstrWithoutRepCharsFinderS1BF.go
@@ -4,10 +4,10 @@ type LongestSubstringWithoutRepCharsFinderS1BF struct {
 }
 
 func (bf LongestSubstringWithoutRepCharsFinderS1BF) findLongestSubstringWithoutRepeatingChars(s string) int {
-	seenChars := make(map[uint8]bool)
 
 	var max int
 	for i := 0; i < len(s); i++ {
+		seenChars := make(map[uint8]bool)
 		var cMax int
 		for j := i; j < len(s); j++ {
 			if _, ok := seenChars[s[j]]; !ok {
@@ -18,7 +18,6 @@ func (bf LongestSubstringWithoutRepCharsFinderS1BF) findLongestSubstringWithoutR
 			}
 		}
 
-		seenChars = make(map[uint8]bool)
 		if cMax > max {
 			max = cMax
 		}

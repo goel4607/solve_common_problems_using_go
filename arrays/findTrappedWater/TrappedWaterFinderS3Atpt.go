3f28e8a5443f7f29d1b2a35fccad95b0a88db364
1mcleancoder <1mcleancoder@gmail.com>
2023-01-16 20:00:41 -0500

    practice impl for Trapped Water Finder problem

---
+++ arrays/findTrappedWater/TrappedWaterFinderS3Atpt.go
@@ -0,0 +1,45 @@
+package findTrappedWater
+
+type TrappedWaterFinderS3Att struct {
+}
+
+func (s3 TrappedWaterFinderS3Att) findTrappedWater(w []int) int {
+	if len(w) <= 2 {
+		return 0
+	}
+
+	var tw, maxL, maxR int
+	left, right := 0, len(w)-1
+
+	for left <= right {
+		if maxL <= maxR {
+			if maxL > 0 {
+				d := maxL - w[left]
+				if d > 0 {
+					tw += d
+				}
+			}
+
+			if maxL < w[left] {
+				maxL = w[left]
+			}
+
+			left++
+		} else {
+			if maxR > 0 {
+				d := maxR - w[right]
+				if d > 0 {
+					tw += d
+				}
+			}
+
+			if maxR < w[right] {
+				maxR = w[right]
+			}
+
+			right--
+		}
+	}
+
+	return tw
+}

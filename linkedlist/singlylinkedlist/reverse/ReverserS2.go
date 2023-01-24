c63bde54f109086a115ead0e3b4020e261561351
1mcleancoder <1mcleancoder@gmail.com>
2023-01-23 21:33:17 -0500

    singly linked list impl for practice

--- linkedlist/singlylinkedlist/reverse/ReverserS2.go
+++ linkedlist/singlylinkedlist/reverse/ReverserS2.go
@@ -6,5 +6,24 @@ type ReverserS2Repeat struct {
 }
 
 func (s2 *ReverserS2Repeat) reverse(n *singlylinkedlist.Node) *singlylinkedlist.Node {
-	return n
+	// 1 -> 2 -> 3 -> 4 -> 5
+	// 1 -> nil and 2 -> 1
+	// prev = nil
+	// curr = n
+	var prev *singlylinkedlist.Node
+
+	curr := n
+	// while n != nil
+	for curr != nil {
+		// save next ptr as temp
+		t := curr.Next
+		// 1 -> prev
+		curr.Next = prev
+		// prev = curr
+		prev = curr
+		// curr = temp i.e 2
+		curr = t
+	}
+	// return prev
+	return prev
 }

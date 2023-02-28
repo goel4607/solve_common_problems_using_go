da0b32265761e3da0a3e305f4569eaf54fa03038
1mcleancoder <1mcleancoder@gmail.com>
2023-02-27 23:09:59 -0500

    dfs impl in 2D Array

---
+++ twoDArr/dfs/Interface_test.go
@@ -0,0 +1,64 @@
+package dfs
+
+import (
+	"github.com/solve_common_problems_using_go"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var (
+	impls = make([]Interface, 0)
+)
+
+type InterfaceTest struct {
+	msg string
+	in  [][]int
+	out []int
+}
+
+func getTests() []InterfaceTest {
+	return []InterfaceTest{
+		{
+			msg: "5 by 5 array, with inc elements on each row",
+			in: [][]int{
+				{1, 2, 3, 4, 5},
+				{6, 7, 8, 9, 10},
+				{11, 12, 13, 14, 15},
+				{16, 17, 18, 19, 20},
+			},
+			out: []int{1, 2, 3, 4, 5, 10, 15, 20, 19, 14, 9, 8, 13, 18, 17, 12, 7, 6, 11, 16},
+		},
+	}
+}
+
+func TestInterface(t *testing.T) {
+	impls = append(
+		impls,
+		Soln1{},
+	)
+
+	tests := getTests()
+	bytes, err := os.ReadFile("problem.md")
+	assert.Nil(t, err)
+	t.Logf("Problem: %s [#tests=%d]", bytes, len(tests))
+
+	assert.NotEmptyf(t, impls, "no implemantations present!!")
+	for _, impl := range impls {
+		t.Logf("Using: %T", impl)
+
+		for i, tt := range tests {
+
+			a := impl.DFS(tt.in)
+
+			var pOrF string
+			if assert.Equal(t, tt.out, a) {
+				pOrF = solve_common_problems_using_go.Passed
+			} else {
+				pOrF = solve_common_problems_using_go.Failed
+			}
+
+			t.Logf("test # %d, %s input=%v and expected output=%v, %v", i+1, pOrF, tt.in, tt.out, tt.msg)
+		}
+	}
+}
---
+++ twoDArr/dfs/Soln1.go
@@ -0,0 +1,66 @@
+package dfs
+
+type Soln1 struct {
+}
+
+type Position struct {
+	row int
+	col int
+}
+
+const (
+	UP = iota
+	RIGHT
+	DOWN
+	LEFT
+)
+
+var DIRECTIONS = []Position{
+	UP:    {-1, 0},
+	RIGHT: {0, 1},
+	DOWN:  {+1, 0},
+	LEFT:  {0, -1},
+}
+
+func (s Soln1) DFS(arr [][]int) []int {
+	visitedPos := s.initVisitedPos(arr)
+	r := make([]int, 0, len(arr)*len(arr))
+	r = append(r, arr[0][0])
+	visitedPos[0][0] = true
+	return s.visitDFS(arr, Position{0, 0}, visitedPos, r)
+}
+
+func (s Soln1) initVisitedPos(arr [][]int) [][]bool {
+	visitedPos := make([][]bool, len(arr), len(arr))
+	for i := range visitedPos {
+		visitedPos[i] = make([]bool, len(arr[i]), len(arr[i]))
+	}
+
+	return visitedPos
+}
+
+func (s Soln1) visitDFS(arr [][]int, current Position, visitedPos [][]bool, r []int) []int {
+	for _, d := range DIRECTIONS {
+		p := s.getEventualPosition(arr, current, d)
+		if p != nil && !visitedPos[p.row][p.col] {
+			visitedPos[p.row][p.col] = true
+			r = append(r, arr[p.row][p.col])
+			r = s.visitDFS(arr, *p, visitedPos, r)
+		}
+	}
+
+	return r
+}
+
+func (s Soln1) getEventualPosition(arr [][]int, current, d Position) *Position {
+	r := current.row + d.row
+	if r >= 0 && r < len(arr) {
+		c := current.col + d.col
+		if c >= 0 && c < len(arr[r]) {
+			return &Position{r, c}
+		}
+		return nil
+	}
+
+	return nil
+}
